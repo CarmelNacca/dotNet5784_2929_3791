6612f3ecbccae1214adbc0fc7e6021db07cbd7a6
CARMEL NACCA <carmelc2244@gmail.com>
2023-12-28 17:51:14 +0200

    welcome function

---
+++ Stage0/Program3791.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3791();
+            Welcome2929();
+            Console.ReadKey();
+
+        }
+
+        private static void Welcome2929()
+        {
+        }
+
+        private static void Welcome3791()
+        {
+            Console.WriteLine("Enter your name");
+            string? name1 = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my firt console application", name1);
+        }
+    }
+}
