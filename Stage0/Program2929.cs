8e568fe29ff35addc963f18326aa31653aa062e9
Michal Toledano <mictoled@acad.jct.ac.il>
2023-12-28 18:32:46 +0200

    Welcome2929 function

---
+++ Stage0/Program2929.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Targil0
+{
+	partial class Program
+	{
+		static  void Welcome2929()
+		{
+			Console.WriteLine("i am here!");
+
+		}
+	}
+}
+
