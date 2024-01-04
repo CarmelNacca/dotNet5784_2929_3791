e1e8a2dd698724aa0ae654da0172f23bf1d31529
CARMEL NACCA <carmelc2244@gmail.com>
2024-01-04 20:19:33 +0200

    stage 1 Chapters 3-4

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,5 +1,16 @@
 ﻿
 
 namespace DO;
+/// <summary>
+/// The difficulty level of the task - defines the minimum 
+/// engineer level that can work on it.
+/// </summary>
+public enum Expirience
+{
+    Beginner,
+    AdvancedBeginner,
+    Intermediate,
+    Advanced,
+    Expert
 
-
+}
---
+++ DalFacade/DO/Worker.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace DO;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id">Unique ID number</param>
+/// <param name="Name">Worker's name (full name)</param>
+/// <param name="Email"></param>
+/// <param name="Cost">cost per hour</param>
+/// <param name="Level">Worker level</param>
+public record Worker
+(
+    int Id,
+    
+    double Cost,
+    Expirience Level,
+    string? Name=null,
+    string? Email=null
+
+
+    )
+{
+public Worker(): this(0, 0,0) { }////empty ctor for stage 3
+
+
+}
