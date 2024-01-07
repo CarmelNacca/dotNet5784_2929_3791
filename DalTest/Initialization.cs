2f7355e473342453931140e05404a55219357efe
CARMEL NACCA <carmelc2244@gmail.com>
2024-01-07 15:26:51 +0200

    part 8

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+public static class Initialization
+{
+    private static IWorker? s_dalWorker;
+    private static ITask? s_dalTask;
+    private static IDependency? s_dalDependency;
+
+    private static readonly Random s_rand = new();
+private static void creatTask()
+{
+
+
+} private static void creatWorker()
+    {
+
+
+    }
+ private static void creatDependency() 
+    {
+
+    }
+}
+
