3d235762e5d859f556cb4d9c9e41cdad98b48de1
Michal Toledano <mictoled@acad.jct.ac.il>
2024-01-07 14:33:31 +0200

    part 5-6

---
+++ DalList/TaskImplementation.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Dal;
+
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class TaskImplementation : ITask
+{
+    public int Create(Task item)
+    {
+        int id = DataSource.Config.NextTaskId;
+        Task copy = item with { Id = id };
+        DataSource.Tasks.Add(copy);
+        return id;
+    }
+
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task? Read(int id)
+    {
+        Task? task1 = null;
+        for (int i = 0; i < DataSource.Tasks.Count; i++)
+        {
+            if (DataSource.Tasks[i].Id == id)
+            {
+                task1 = DataSource.Tasks[i];
+                break;
+            }
+           
+        }
+        return task1;
+    }
+
+    public List<ITask> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Task item)
+    {
+
+        if (Read(item.Id) is not null)
+            throw new Exception($"Task with ID={item.Id} not exists");
+        Delete(item.Id);
+        DataSource.Tasks.Add(item);
+    }
+}
---
+++ DalList/WorkerImplementation.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Dal;
+
+using DalApi;
+using DO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class WorkerImplementation : IWorker
+{
+    public int Create(Worker item)
+    {
+        if (Read(item.Id) is not null)
+            throw new Exception($"Worker with ID={item.Id} already exists");
+        DataSource.Workers.Add(item);
+        return item.Id;
+    }
+
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Worker? Read(int id)
+    {
+        Worker? worker1 = null;
+        for (int i = 0; i < DataSource.Workers.Count; i++)
+        {
+            if (DataSource.Workers[i].Id == id)
+            { 
+                worker1 = DataSource.Workers[i];
+                break;
+            }
+        }
+        return worker1;
+    }
+
+    public List<IWorker> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Worker item)
+    {
+        if (Read(item.Id) is not null)
+            throw new Exception($"Worker with ID={item.Id} not exists");
+        Delete(item.Id);
+        DataSource.Workers.Add(item);
+    }
+}
+
