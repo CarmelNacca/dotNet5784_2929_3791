557e48a99d1d964ee0917dbf9ebceb186a93c163
CARMEL NACCA <carmelc2244@gmail.com>
2024-01-11 13:52:01 +0200

    2

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,17 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.DalApi
+{
+    public interface IDal
+    {
+        IDependency Dependency { get; }
+        ITask Task { get; }
+        IWorker Worker { get; }
+
+    }
+}
