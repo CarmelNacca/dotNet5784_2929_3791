e51320d714e11e7a4c097e349a311c3b098a0a10
CARMEL NACCA <carmelc2244@gmail.com>
2024-01-23 14:00:56 +0200

    stage 3 .6

---
+++ DalXml/TaskImplementation.cs
@@ -0,0 +1,52 @@
+﻿
+
+using DalApi;
+using System.Data.Common;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Dal;
+
+internal class TaskImplementation:ITask
+{
+    readonly string s_tasks_xml = "tasks";
+   
+
+    public int Create(DO.Task item)
+    {
+      
+
+        //    int id = DataSource.Config.NextTaskId;
+        //    Task copy = item with { Id = id };
+        //    DataSource.Tasks.Add(copy);
+        //    return id;
+        //}
+    }
+
+
+    public void Delete(int id)
+    {
+       
+    }
+
+    public DO.Task? Read(int id)
+    {
+       
+
+    }
+
+    public DO.Task? Read(Func<DO.Task, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<DO.Task?> ReadAll(Func<DO.Task, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(DO.Task item)
+    {
+        throw new NotImplementedException();
+    }
+}
