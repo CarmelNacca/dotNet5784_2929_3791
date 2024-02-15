14ea5f201cf5df58353a4be21a58477d6aaa7191
CARMEL NACCA <carmelc2244@gmail.com>
2024-02-15 18:18:54 +0200

    Stage 5

---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Dal;
+using PL.Worker;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnWorker(object sender, RoutedEventArgs e)
+        {
+            new WorkerListWindow().Show();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Would you like to initialize data?"," ", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+
+                   
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Oh well, too bad!", "My App");
+                    break;
+                case MessageBoxResult.Cancel:
+                    MessageBox.Show("Nevermind then...", "My App");
+                    break;
+            }
+        }
+    }
+}
