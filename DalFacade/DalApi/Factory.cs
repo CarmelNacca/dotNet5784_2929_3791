1134c602a362a80766b500727890ed8d5bcfbd1d
CARMEL NACCA <carmelc2244@gmail.com>
2024-02-16 01:25:24 +0200

    fix

--- DalFacade/DalApi/Factory.cs
+++ DalFacade/DalApi/Factory.cs
@@ -7,7 +7,7 @@ public static class Factory
     public static IDal Get
     {
         get
-        {
+        { 
             string dalType = s_dalName ?? throw new DalConfigException($"DAL name is not extracted from the configuration");
             DalImplementation dal = s_dalPackages[dalType] ?? throw new DalConfigException($"Package for {dalType} is not found in packages list in dal-config.xml");
 
