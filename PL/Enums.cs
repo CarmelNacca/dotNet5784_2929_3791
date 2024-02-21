a504fa693e1234955200b84285fa15a231774763
CARMEL NACCA <carmelc2244@gmail.com>
2024-02-21 16:56:45 +0200

    stage 5 part 7

---
+++ PL/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL;
+
+internal class Enums : IEnumerable
+{
+static readonly IEnumerable<BO.Expirience> s_enums =
+(Enum.GetValues(typeof(BO.Expirience)) as IEnumerable<BO.Expirience>)!;
+
+public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
