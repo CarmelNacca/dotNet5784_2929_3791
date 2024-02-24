ca4e523f96e9dfb756d1641e72a378b4a4ee560b
Michal Toledano <mictoled@acad.jct.ac.il>
2024-02-24 20:56:38 +0200

    almost the end

---
+++ PL/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{ 
+internal class WorkerLevel : IEnumerable
+{
+    static readonly IEnumerable<BO.ExpiriencePl> s_enums =
+(Enum.GetValues(typeof(BO.ExpiriencePl)) as IEnumerable<BO.ExpiriencePl>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+    internal class Level : IEnumerable
+    {
+        static readonly IEnumerable<BO.Expirience> s_enums =
+    (Enum.GetValues(typeof(BO.Expirience)) as IEnumerable<BO.Expirience>)!;
+
+        public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+    }
+};
