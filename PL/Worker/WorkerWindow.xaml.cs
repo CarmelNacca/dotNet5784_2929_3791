0074c064f1b146e82d1c8aa0549300371023e67e
Michal Toledano <mictoled@acad.jct.ac.il>
2024-02-24 20:56:38 +0200

    almost the end

--- PL/Worker/WorkerWindow.xaml.cs
+++ PL/Worker/WorkerWindow.xaml.cs
@@ -1,32 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PL.Worker
 {
     /// <summary>
-    /// Interaction logic for WorkerWindow.xaml
+    ///
     /// </summary>
+
     public partial class WorkerWindow : Window
     {
-        public WorkerWindow()
+        static readonly BlApi.IBL s_bl = BlApi.Factory.Get();
+        public BO.Worker? WorkerPL
         {
-            InitializeComponent();
+            get { return (BO.Worker)GetValue(WorkerProperty); }
+            set { SetValue(WorkerProperty, value); }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public static readonly DependencyProperty WorkerProperty =
+           DependencyProperty.Register("WorkerPL", typeof(BO.Worker),
+               typeof(WorkerWindow), new PropertyMetadata(null));
+        public WorkerWindow(int id = 0)
         {
+            InitializeComponent();
+            try
+            {
+                WorkerPL = ((id != 0) ? s_bl.Worker.Read(id)! : new BO.Worker { Id = 0, Name = " ", Email = " ", Level = 0, Cost = 0, Task = null });
+            }
+            catch (BO.BlDoesNotExistException ex)
+            {
+                WorkerPL = null;
 
+                MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+        }
+
+
+       
+        private void Button_Click_AddUpdate(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button)!.Content.ToString() == "Add")
+                try
+                {
+                    int? id = s_bl.Worker.Add(WorkerPL!);
+                    MessageBox.Show($"worker {id} was successfully added", "success", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    this.Close();
+                }
+                catch (BO.BlAlreadyExistsException ex)
+                {
+                    MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                }
+            else
+            {
+                try
+                {
+                    s_bl.Worker.Update(WorkerPL!);
+                    MessageBox.Show($"worker {WorkerPL?.Id} was successfully updeted", "success", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    this.Close();
+                }
+                catch (BO.BlDoesNotExistException ex)
+                {
+                    MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                } 
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                }
+            }
+
+            this.Close();
         }
     }
 }
