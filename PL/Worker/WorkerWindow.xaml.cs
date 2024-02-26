2ead4c8a6f9b356fa5b5243c1ca0afdd8117805b
Michal Toledano <mictoled@acad.jct.ac.il>
2024-02-26 02:11:19 +0200

    try

--- PL/Worker/WorkerWindow.xaml.cs
+++ PL/Worker/WorkerWindow.xaml.cs
@@ -32,7 +32,7 @@ public partial class WorkerWindow : Window
 
             MessageBox.Show(ex.Message, "operation faild", MessageBoxButton.OK, MessageBoxImage.Exclamation);
             this.Close();
-            
+             
             
         }
     }
