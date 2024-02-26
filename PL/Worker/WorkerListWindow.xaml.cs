032666afe3f73b77034730b2e7604f366fc1dd74
CARMEL NACCA <carmelc2244@gmail.com>
2024-02-26 02:09:06 +0200

    try

--- PL/Worker/WorkerListWindow.xaml.cs
+++ PL/Worker/WorkerListWindow.xaml.cs
@@ -12,19 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace PL.Worker
 {
@@ -49,7 +37,8 @@ namespace PL.Worker
         public static readonly DependencyProperty WorkerListProperty =
             DependencyProperty.Register("WorkerList", typeof(IEnumerable<BO.Worker>), typeof(WorkerListWindow), new PropertyMetadata(null));
 
-       
+
+        
     }
 
 
