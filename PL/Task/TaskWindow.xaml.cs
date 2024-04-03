0fb6ee1fb1a69ad03777ecc15e411dfb3c967e09
CARMEL NACCA <carmelc2244@gmail.com>
2024-04-04 00:14:33 +0300

    .

--- PL/Task/TaskWindow.xaml.cs
+++ PL/Task/TaskWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL.Task//carmel
+namespace PL.Task
 {
     /// <summary>
     /// Interaction logic for TaskWindow.xaml
